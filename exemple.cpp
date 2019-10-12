31affbae11f9fe58ba5d413e48648258baa545af
MickaelBlet <mickael.blet@gmail.com>
2019-10-12 23:48:56 +0200

    update with new syntax

---
+++ exemple.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+struct Custom {
+    int child;
+};
+
+int main(int argc, char **argv) {
+    Custom  newType; // Type
+
+    newType.child = 42;
+    std::cout << "Namespace and child: " << newType.child << std::endl;
+    std::cout << argv[0] << ": number of param: " << argc << std::endl;
+    return 0;
+}
