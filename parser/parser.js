ecf7666ebeeea5659da418f0d71bbc9f68494036
Mickaël Blet <mickael.blet@gmail.com>
2021-09-24 16:26:38 +0200

    Update parser.js

--- parser/parser.js
+++ parser/parser.js
@@ -5,6 +5,7 @@ class Parser {
         this.activeEditor;
         this.text;
         this.decoration;
+        this.logger = vscode.window.createOutputChannel("MBLET syntax cpp");
         this.ranges = [];
         this.loadConfigurations(contributions);
     }
@@ -16,6 +17,10 @@ class Parser {
     // load configuration from contributions
     loadConfigurations(contributions) {
         this.decoration = vscode.window.createTextEditorDecorationType(contributions.parameters);
+    }
+
+    log(text) {
+        this.logger.appendLine(text)
     }
 
     resetDecorations(activeEditor) {
@@ -72,7 +77,34 @@ class Parser {
         // replace define line
         text = text.replace(/#[^]*?(?:(?<!\\)$)/gm, replacer);
         // replace compiler macro
-        text = text.replace(/__[a-zA_Z]+__[^)]*[)\s]*/gm, replacer);
+        text = text.replace(/(?:__[a-z_A-Z]+__|throw|noexcept)\s*[(][^);]*(?:[)\s]+)/gm, replacer);
+        // replace enum
+        text = text.replace(/\benum\b\s*(?:struct|class)?\s*(?:\b[a-z_A-Z0-9]+\b)?\s*(?:[:][^]*?(?:}\s*;)+|{[^]*?(?:}\s*;))/gm, replacer);
+
+        let matches = [...text.matchAll(/\btemplate\b\s*</gm)];
+        matches.forEach(match => {
+            let level = 0;
+            let start;
+            let end;
+            for (let i = match.index ; i < text.length ; i++) {
+                if (text[i] == "<") {
+                    level++;
+                    if (level == 1)
+                        start = i;
+                }
+                else if (text[i] == ">") {
+                    level--;
+                    if (level == 0) {
+                        end = i;
+                        text = this.replaceBySpace(text,match.index,end+1);
+                        break;
+                    }
+                }
+            }
+        });
+
+        // this.log(text)
+
         return text;
     }
 
@@ -116,33 +148,32 @@ class Parser {
         return text;
     }
 
-    getOpenParenthesisIndex(index) {
-        let i = this.text.indexOf("(", index);
-        if (i < 0) {
+    getParenthesisIndex(index) {
+        let startParenthesis = this.text.indexOf("(", index);
+        if (startParenthesis < 0) {
             return null;
         }
-        return i;
-    }
 
-    getCloseParenthesisIndex(index) {
-        let isClose = false;
-        let level = 0;
-        for (let i = index ; i < this.text.length ; i++) {
-            if (level == 1 && this.text[i] == ")") {
-                isClose = true;
-                level = 0;
+        let level = 1;
+        for (let i = startParenthesis + 1 ; i < this.text.length ; i++) {
+            if (this.text[i] == ":" && this.text[i+1] == ":") {
+                i++;
             }
-            else if (isClose === true && (this.text[i] == ":" || this.text[i] == "{" || this.text[i] == ";")) {
-                return i;
+            else if (level == 0 && (this.text[i] == ":" || this.text[i] == "{" || this.text[i] == ";")) {
+                return [startParenthesis, i];
             }
             else if (level > 0 && this.text[i] == ")") {
                 level--;
             }
+            else if (level == 0 && this.text[i] == "(") {
+                startParenthesis = i;
+                level = 1;
+            }
             else if (this.text[i] == "(") {
-                isClose = false;
                 level++;
             }
         }
+
         return null;
     }
 
@@ -178,7 +209,7 @@ class Parser {
     searchPrototypes(start, end) {
         let words = [];
 
-        let regEx = /([a-z_A-Z0-9<>]+\s*[&*]*\s*(?:[(][&*]*)?)\b([a-z_A-Z][a-z_A-Z0-9]*)\s*(?:,|=[^,]*(?:,|[)(])|\[[^\]]*\]|[)(])\s*/gm;
+        let regEx = /([a-z_A-Z0-9<>]+(?:::)?\s*[&*]*\s*(?:[(][&*]*)?)\b([a-z_A-Z][a-z_A-Z0-9]*)\s*(?:,|=[^,]*(?:,|[)(])|\[[^\]]*\]|[)(])\s*/gm;
         let text = this.text.substr(start, end - start);
         text = this.containerHidden(text);
         let search;
@@ -222,12 +253,10 @@ class Parser {
     // search all function in text document
     searchFunctions() {
         let endParenthesis = 0;
-        let startParenthesis;
-        while (startParenthesis = this.getOpenParenthesisIndex(endParenthesis)) {
-            endParenthesis = this.getCloseParenthesisIndex(startParenthesis);
-            if (endParenthesis == null) {
-                return ;
-            }
+        let parenthesis;
+        while (parenthesis = this.getParenthesisIndex(endParenthesis)) {
+            let startParenthesis = parenthesis[0];
+            endParenthesis = parenthesis[1];
             let startBrace = this.getOpenBraceIndex(endParenthesis);
             if (startBrace == null) {
                 this.searchPrototypes(startParenthesis, endParenthesis)
